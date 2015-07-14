8bb459c29089b3c3b9d5ba1c22af0ef9acc3eb70
Rowan Manning <rowan.manning@nature.com>
2015-07-14 17:01:30 +0100

    Give a crab

---
+++ scripts/crab.js
@@ -0,0 +1,17 @@
+// Description:
+//   Crabs for all.
+//
+// Commands:
+//   hubot crab me - Get a crab
+
+'use strict';
+
+module.exports = initScript;
+
+function initScript (robot) {
+	robot.respond(/crab me/i, giveACrab);
+}
+
+function giveACrab (message) {
+	message.send(':crab:');
+}
