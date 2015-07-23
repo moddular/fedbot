3d0467159661600a57e16c5547ad0c8790b1c540
Rowan Manning <rowan.manning@nature.com>
2015-07-23 16:48:57 +0100

    Goat me

---
+++ scripts/goat-me.js
@@ -0,0 +1,16 @@
+// Description:
+//   Get someone's goat
+//
+// Commands:
+//   hubot goat me - Get someone's goat
+
+'use strict';
+
+module.exports = robotRespond(/goat me/i, [
+	':goat:',
+	'http://i.imgur.com/fgUmFyA.gif',
+	'http://i.imgur.com/JEgjEBL.gif',
+	'http://media.giphy.com/media/qpmA24MOavdGU/giphy.gif',
+	'http://media.giphy.com/media/Vw2uzhE0Bwfks/giphy.gif',
+	'http://www.doseoffunny.com/wp-content/uploads/2014/04/i.chzbgr.gif'
+]);
